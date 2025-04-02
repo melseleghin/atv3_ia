f3dc558b23f615544af6f22e0c9179b9dd6f5736
melseleghin <melseleghin@gmail.com>
2025-04-02 09:01:57 -0300

    removendo o metodo elbow para o algoritmo bi-secting

--- bi-secting/bi_secting_kmeans.py
+++ bi-secting/bi_secting_kmeans.py
@@ -5,40 +5,17 @@ from sklearn.decomposition import PCA
 
 
 class BisectingKMeansAnalytic:
-
     def __init__(self, dataset):
         self.dataset = dataset
         self.reduced_data = None
-        self.list_clusters = []
-        self.list_inertias = []
-        self.qtd_clusters_otima = 0
+        self.qtd_clusters_otima = 2   
         self.target = None
 
-    def bisecting_kmeans(self, qtd_clusters):
-        return BisectingKMeans(n_clusters=qtd_clusters).fit(self.dataset.data)
-
-    @staticmethod
-    def inercia(model):
-        return model.inertia_
-
-    def run_bisecting_kmeans_with_dif_knumbers(self):
-        for i in range(1, 11):
-            self.list_clusters.append(i)
-            model = self.bisecting_kmeans(i)
-            inertia = self.inercia(model)
-            self.list_inertias.append(inertia)
-
-    def plot_elbow_method(self):
-        plt.plot(self.list_clusters, self.list_inertias)
-        plt.xlabel('Number of Clusters (k)')
-        plt.ylabel('Inertia')
-        plt.title('Elbow Method with inertia (Bisecting K-Means)')
-        plt.xticks(self.list_clusters)
-        plt.grid()
-        plt.show()
+    def bisecting_kmeans(self):
+        return BisectingKMeans(n_clusters=self.qtd_clusters_otima).fit(self.dataset.data)
 
     def apply_pca(self):
-        model = BisectingKMeans(n_clusters=self.qtd_clusters_otima).fit(self.dataset.data)
+        model = self.bisecting_kmeans()
         self.target = model.labels_
         self.reduced_data = PCA(n_components=2).fit_transform(self.dataset.data)
 
@@ -51,6 +28,7 @@ class BisectingKMeansAnalytic:
             c=self.target,
             s=40
         )
+        plt.title(f'Bisecting K-Means Clustering (k={self.qtd_clusters_otima})')
         plt.show()
 
 
@@ -59,21 +37,12 @@ if __name__ == "__main__":
     print("### BASE DE DADOS IRIS ###")
     iris = load_iris()
     bkm_iris = BisectingKMeansAnalytic(iris)
-    bkm_iris.run_bisecting_kmeans_with_dif_knumbers()
-    bkm_iris.plot_elbow_method()
-    bkm_iris.qtd_clusters_otima = int(input("Qual o número de k ótimo apontado pelo elbow method? (no nosso caso, é 3):  "))
-    print(f"Plotando gráfico com {bkm_iris.qtd_clusters_otima} clusters...")
     bkm_iris.apply_pca()
     bkm_iris.plot_bisecting_kmeans_clustering()
 
     # PARA BASE WINE:
     print("### BASE DE DADOS WINE ###")
     wine = load_wine()
-    bkm_wine = BisectingKMeansA
-    nalytic(wine)
-    bkm_wine.run_bisecting_kmeans_with_dif_knumbers()
-    bkm_wine.plot_elbow_method()
-    bkm_wine.qtd_clusters_otima = int(input("Qual o número de k ótimo apontado pelo elbow method? (no nosso caso, é 3):  "))
-    print(f"Plotando gráfico com {bkm_wine.qtd_clusters_otima} clusters...")
+    bkm_wine = BisectingKMeansAnalytic(wine)
     bkm_wine.apply_pca()
     bkm_wine.plot_bisecting_kmeans_clustering()
